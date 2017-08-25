dc732edfdbac466877c71d561be83d905cd11ce1
Ilya Gruzinov <igruzinov@yandex.ru>
2017-08-25 15:36:46 +0300

    Basic API app

---
+++ api/app/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Api;
+
+class User
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
